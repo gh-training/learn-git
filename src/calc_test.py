a5fe01a99fa50b55342334280d196d8f76f998ea
Mike North <michael.l.north@gmail.com>
2021-02-07 18:34:56 -0800

    Add missing calc utility unit tests

---
+++ src/calc_test.py
@@ -0,0 +1,15 @@
+import unittest
+from calc import add, sub 
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_add_2arg(self):
+        # Make sure 3 + 4 = 7
+        self.assertEqual(add(3, 4), 7, 'adding three and four')
+   
+    def test_sub_2arg(self):
+        # Make sure 4 - 3 = 1
+        self.assertEqual(sub(4, 3), 1, 'subtracting three from four')
+
+if __name__ == '__main__':
+    unittest.main()
