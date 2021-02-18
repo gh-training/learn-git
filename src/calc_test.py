39edddde038e4e8ac7ce185be4b1c0fc223a2bb6
Mike North <michael.l.north@gmail.com>
2021-02-18 13:04:50 -0800

    Support three-argument subtraction (#8)
    Users have reported that 3-arg subtraction doesn't work
    We forgot to implement it!

--- src/calc_test.py
+++ src/calc_test.py
@@ -6,10 +6,13 @@ class TestStringMethods(unittest.TestCase):
     def test_add_2arg(self):
         # Make sure 3 + 4 = 7
         self.assertEqual(add(3, 4), 7, 'adding three and four')
-   
+
     def test_sub_2arg(self):
         # Make sure 4 - 3 = 1
         self.assertEqual(sub(4, 3), 1, 'subtracting three from four')
+
+    def test_sub_3argument_variant(self):
+        self.assertEqual(sub(4, 3, 1), 0, 'subtracting one and three from four')
 
 if __name__ == '__main__':
     unittest.main()
