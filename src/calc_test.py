c31685cc652709de980b535ce254e4366214ed60
Andrea Yang <anyang@linkedin.com>
2021-02-22 15:22:13 -0800

    BUGFIX: 3-arg subtraction was not implemented
    The following change allows use of sub like
    
    ```py
    
    sub(5, 4, 1) #0
    ```
    
    We promised customers that we would have this in our initial release, so this is
    a bug, not an enhancement.

--- src/calc_test.py
+++ src/calc_test.py
@@ -11,6 +11,8 @@ class TestStringMethods(unittest.TestCase):
         # Make sure 4 - 3 = 1
         self.assertEqual(sub(4, 3), 1, 'subtracting three from four')
 
+    def test_sub_3arg(self):
+	self.assertEqual(sub(4, 3, 1), 0, 'subtracgint three and one from four')
 
 if __name__ == '__main__':
     unittest.main()
