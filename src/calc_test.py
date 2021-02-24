59f6acd827c6103aea7cb6c2102e3e46cb5945c1
Gurvineet Dhillon <gdhillon@linkedin.com>
2021-02-24 10:40:49 -0800

    First Commit

--- src/calc_test.py
+++ src/calc_test.py
@@ -11,6 +11,10 @@ class TestStringMethods(unittest.TestCase):
         # Make sure 4 - 3 = 1. Test
         self.assertEqual(sub(4, 3), 1, 'subtracting three from four')
 
+    def test_sub_3arg(self):
+        # Make sure 4 - 3 = 1. Test
+        self.assertEqual(sub(4, 3, 1), 0, 'subtracting one from three from four')
+
 
 if __name__ == '__main__':
     unittest.main()
