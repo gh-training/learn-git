f63903e5cd07b695eb9187bd07d61b10126c2645
Snehangshu Karmakar <snkarmakar@linkedin.com>
2021-03-26 17:18:33 +0530

    BUGFIX: 3-arg addition was not impleneted
    The following change allows use of add like
    
    ```py
    
    add(4, 5, 1) #10
    ```
    
    We promised customer that we would have this in our
    initial release, so this is a bug, not an enhancement.

--- src/calc_test.py
+++ src/calc_test.py
@@ -10,13 +10,6 @@ class TestStringMethods(unittest.TestCase):
     def test_add_3arg(self):
         self.assertEqual(add(3, 5, 2), 10, 'adding three, five and two')
 
-    def test_add_zeroes(self):
-        self.assertEqual(add(0, 0, 0), 0 , 'adding zeroes together is zero')
-
-    def test_sub_2arg(self):
-        # Make sure 4 - 3 = 1
-        self.assertEqual(sub(4, 3), 1, 'subtracting three from four')
-
 
 if __name__ == '__main__':
     unittest.main()
