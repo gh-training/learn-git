f9f2a9ec32305f4a60b97260ad91dba9a55e4fd0
Peter Tong <ptong@linkedin.com>
2021-08-19 16:29:03 -0700

    BUGFIX: implement 3-arg subtraction

--- src/calc_test.py
+++ src/calc_test.py
@@ -17,6 +17,9 @@ class TestStringMethods(unittest.TestCase):
         # Make sure 4 - 3 = 1
         self.assertEqual(sub(4, 3), 1, 'subtracting three from four')
 
+    def test sub_3arg(self):
+        self.assertEqual(sub(4, 3, 1), 0, 'subtractingthree and one from four')
+
 
 if __name__ == '__main__':
     unittest.main()
