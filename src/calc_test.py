b33081e323fc829e05a2648320f2d42cf6a6d45e
Max Miranda <maxmiranda@berkeley.edu>
2021-12-02 17:22:39 -0800

    Apply suggestions from code review

--- src/calc_test.py
+++ src/calc_test.py
@@ -18,7 +18,7 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(sub(4, 3), 1, 'subtracting three from four')
 
     def test_sub_3arg(self):
-	self.assertEqual(sub(4, 3, 1), 0, 'subtracting three and one from four')
+	self.assertEqual(sub(4, 3, 1), 0, 'subtracting three and one from five')
 
 if __name__ == '__main__':
     unittest.main()
