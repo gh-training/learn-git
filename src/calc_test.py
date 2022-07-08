d0b826fb426bff49a4fd73d0111e9cbb6f3a9984
Laura Bennett <lbennett@lbennett-mn1.linkedin.biz>
2022-07-07 18:14:04 -0700

    add missing tests(zero case)

--- src/calc_test.py
+++ src/calc_test.py
@@ -20,5 +20,8 @@ class TestStringMethods(unittest.TestCase):
     def test_sub_3arg(self):
         self.assertEqual(sub(4, 3, 1), 0, 'subtracting three from one from four')
 
+    def test_sub_3arg_zeros(self):
+         self.assertEqual(sub(0, 0, 0), 0, 'zero minus zero minus zero is zero')
+
 if __name__ == '__main__':
     unittest.main()
